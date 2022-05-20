4ea4a702637e27c582c1aac61319522114b29654
carapaceonbear <seerhan17@gmail.com>
2022-05-20 16:06:12 +0100

    calculation working in scrap file

--- scratch.js
+++ scratch.js
@@ -1,11 +1,14 @@
-const startingFormula = "((4^2)+2(3*4))√2^4-(1+1)(1+1)";
+const startingFormula = "((4^2.5).5+2(3*4))√2^4-(1+1)(1.5+1)";
 let workingFormula = "";
 
-const numberCheck = new RegExp("[0-9.]");
+const numberCheck = new RegExp("[0-9]");
+const numberDecimalCheck = new RegExp("[0-9.]");
+const operators = ["*", "/", "+", "-"];
 
 let bracketStart = 0;
 let bracketEnd = 0;
 let bracketSegment = "";
+let currentCount = 0;
 let preceder = 0;
 let follower = 0;
 
@@ -14,14 +17,14 @@ const calculatePowers = (segment) => {
     let powerIndex = segment.indexOf("^");
     // get the surrounding numbers
     for (let i = powerIndex-1; i >= 0; i--) {
-        if (numberCheck.test(segment[i])) {
+        if (numberDecimalCheck.test(segment[i])) {
             preceder = i;
         } else {
             break;
         }
     }
     for (let j = powerIndex+1; j < segment.length; j++) {
-        if (numberCheck.test(segment[j])) {
+        if (numberDecimalCheck.test(segment[j])) {
             follower = j;
         } else {
             break;
@@ -31,6 +34,60 @@ const calculatePowers = (segment) => {
     let preNumber = parseFloat(segment.slice(preceder, powerIndex));
     let postNumber = parseFloat(segment.slice(powerIndex+1, follower+1));
     return Math.pow(preNumber, postNumber);
+}
+
+const calculateRoots = (segment) => {
+    // get the first root in the current segment
+    let rootIndex = segment.indexOf("√");
+    // get the following numbers
+    for (let i = rootIndex+1; i < segment.length; i++) {
+        if (numberDecimalCheck.test(segment[i])) {
+            follower = i;
+        } else {
+            break;
+        }
+    }
+    preceder = rootIndex; // need to update this to slice the result back in
+    // run the calculation
+    let postNumber = parseFloat(segment.slice(rootIndex+1, follower+1));
+    return Math.sqrt(postNumber);
+}
+
+const calculateBasic = (segment, symbol) => {
+    let operator = symbol;
+    // get the first operator in the current segment
+    let operatorIndex = segment.indexOf(operator);
+    // get the surrounding numbers
+    for (let i = operatorIndex-1; i >= 0; i--) {
+        if (numberDecimalCheck.test(segment[i])) {
+            preceder = i;
+        } else {
+            break;
+        }
+    }
+    for (let j = operatorIndex+1; j < segment.length; j++) {
+        if (numberDecimalCheck.test(segment[j])) {
+            follower = j;
+        } else {
+            break;
+        }
+    }  
+    // run the calculation
+    let preNumber = parseFloat(segment.slice(preceder, operatorIndex));
+    let postNumber = parseFloat(segment.slice(operatorIndex+1, follower+1));
+    switch(operator) {
+        case "*":
+            return (preNumber * postNumber);
+        case "/":
+            return (preNumber / postNumber);
+        case "+":
+            return (preNumber + postNumber);
+        case "-":
+            return (preNumber - postNumber);
+        default:
+            return null;
+    }
+
 }
 
 
@@ -51,16 +108,42 @@ const performCalculation = (segment) => {
         `${workingSegment.slice(0, preceder)}${powerResult}${workingSegment.slice(follower+1, workingSegment.length)}`;
     };
 
+    // ROOTS
+    // count how many
+    let rootsCount = 0;
+    for (let i = 0; i < workingSegment.length; i++) {
+        if (workingSegment[i] == "√") {
+            rootsCount ++;
+        };
+    };
+    // run the roots function for each
+    for (let j = 1; j <= rootsCount; j++) {
+        let rootsResult = calculateRoots(workingSegment);
+        // if a number directly precedes, insert "*"
+        if (numberCheck.test(workingSegment[preceder-1])) {
+           rootsResult = `*${rootsResult}`;
+        }
+        workingSegment = 
+        `${workingSegment.slice(0, preceder)}${rootsResult}${workingSegment.slice(follower+1, workingSegment.length)}`;
+    };
 
-
-
-    // roots
-    // console.log(segment.indexOf("√"));
-
-
+    operators.forEach((operator) => {
+        currentCount = 0;
+        for (let i = 0; i < workingSegment.length; i++) {
+            if (workingSegment[i] == operator) {
+                currentCount ++;
+            };
+        };
+        for (let j = 1; j <= currentCount; j++) {
+            let basicResult = calculateBasic(workingSegment, operator);
+            workingSegment = 
+            `${workingSegment.slice(0, preceder)}${basicResult}${workingSegment.slice(follower+1, workingSegment.length)}`;
+        };
+    });
 
     return (workingSegment);
 }
+
 
 const bracketSegmenter = (formula) => {
     // select the bracket segment for calculation
